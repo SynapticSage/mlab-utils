9db0047c0660a8ccf35a05f8809ca8a1bf881b5b
Ryan <ryoung@brandeis.edu>
2022-12-23 01:35:45 -0500

    ndb.cellfetch now no longer assumes a requested field exists for each struct. [] is used for null result. this is cast to the modal type found for that field.

--- +ndb/cellfetch.m
+++ +ndb/cellfetch.m
@@ -20,10 +20,23 @@ for index = indices'
     fcount = 0;
     for field = fields(:)'
         fcount = fcount + 1;
-
-        varargout{fcount}{icnt} = y.(field);
-        
+        if isfield(y, field)
+            varargout{fcount}{icnt} = y.(field);
+        end
     end
+end
+
+% Uniform types
+for v = 1:length(varargout)
+    V = varargout{v};
+    types = cellfun(@(x) class(x), V, 'UniformOutput', false);
+    [C,IA,IC] = unique(types);
+    modeIC = mode(IC);
+    C = C(modeIC);
+    C = C{1};
+    inds = cellfun(@(x) isequal([],x), V);
+    V(inds) = {cast([], C)};
+    varargout{v} = V;
 end
 
 varargout{end+1} = indices;
